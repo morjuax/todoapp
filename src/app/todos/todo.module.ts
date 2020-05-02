fadf589adc8d6f58c69f740029b83a3805cb6f7c
Juan Moreno <ext_jcmorenos@falabella.cl>
2020-05-02 18:56:27 -0400

    [INIT] project

---
+++ src/app/todos/todo.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TodoAddComponent } from './components/todo-add/todo-add.component';
+import { TodoFooterComponent } from './components/todo-footer/todo-footer.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoPageComponent } from './components/todo-page/todo-page.component';
+
+
+
+@NgModule({
+  declarations: [TodoAddComponent, TodoFooterComponent, TodoItemComponent, TodoListComponent, TodoPageComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class TodoModule { }
